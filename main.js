f5d980822aba5615b5822ca8a33713f56659a80f
Eveline Guerrero <evelineguerrero23@gmail.com>
2021-12-23 00:44:25 -0500

    commit tree 564d7f1108184464553a823ae902b97038104866

--- main.js
+++ main.js
@@ -3,6 +3,39 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const glyphStates = {
+  "♡": "♥",
+  "♥": "♡"
+};
+
+const colorStates = {
+  "red" : "",
+  "": "red"
+};
+
+const articleHearts = document.querySelectorAll(".like-glyph");
+
+function likeCallback(e) {
+  const heart = e.target;
+  mimicServerCall()
+    .then(function(serverMessage){
+      // STEP 2: Uncomment the 3 lines after the alert.
+      // Here we're using Pillar 1 (DOM Manipulation) to update the screen and
+      // mimicking Pillar 3 (Server Communication) to only update the screen if
+      // the sending of information to the server succeeds.
+      alert("You notified the server!");
+      alert(serverMessage);
+      heart.innerText = glyphStates[heart.innerText];
+      heart.style.color = colorStates[heart.style.color];
+    })
+    .catch(function(error) {
+      alert("Something went wrong!");
+    });
+}
+
+for (const glyph of articleHearts) {
+  glyph.addEventListener("click", likeCallback);
+}
 
 
 
